cde4612515c0b5856bfadae13f19a1ecbaa948a8
Julius Häger <julius_hager@hotmail.com>
2018-07-18 18:33:41 +0200

    Testing some js

--- allajobb/src/App.js
+++ allajobb/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JSONTest from './JSONData'
 
 class App extends Component {
   render() {
+    jsonGetAsync("http://pilot.arbetsformedlingen.se/searchapi/mvp/search?q=systemutvecklare%20i%20Stockholm&offset=0&limit=10&fromdatetime=2018-02-28%2000%3A00%3A01&sortorder=DEFAULT", (data) => {
+      //alert(data.results);
+    });
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +17,33 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Test
+        </p>
+        <JSONTest/>
       </div>
     );
   }
 }
 
 export default App;
+
+function jsonGetAsync(theUrl, success)
+{
+    httpGetAsync(theUrl, (data) => {
+      try {
+        success(JSON.parse(data));
+      } catch (error) {}
+    });
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
